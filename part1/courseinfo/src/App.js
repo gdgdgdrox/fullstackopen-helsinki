55dbeeb3b140b46a27ac88ac749194124cb4cb76
gdgdgdrox <gdfoo94@gmail.com>
2023-07-09 14:11:10 +0800

    initial commit. done part 1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import Header from './Header';
+import Content from './Content';
+import Total from './Total';
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  return (
+    <div>{counter}</div>
+  )
+}
+// const App = () => {
+//   const course = {
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         exercises: 10
+//       },
+//       {
+//         name: 'Using props to pass data',
+//         exercises: 7
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14
+//       }
+//     ]
+//   }
+
+//   return (
+//     <div>
+//       <Header course={course.name}/>
+//       <Content parts={course.parts}/>
+//       <Total parts={course.parts}/>
+//     </div>
+//   )
+// }
+
+export default App
